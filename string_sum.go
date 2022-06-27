9b91ec574493658706df4cd426a8281d25fd20ee
HP-Maksim\Maksim <maksimboikodev@gmail.com>
2022-06-27 15:45:29 +0300

    ex 4.0

--- string_sum.go
+++ string_sum.go
@@ -2,6 +2,9 @@ package string_sum
 
 import (
 	"errors"
+	"fmt"
+	"strconv"
+	"strings"
 )
 
 //use these errors as appropriate, wrapping them with fmt.Errorf function
@@ -23,5 +26,44 @@ var (
 // Use the errors defined above as described, again wrapping into fmt.Errorf
 
 func StringSum(input string) (output string, err error) {
-	return "", nil
+	input = strings.NewReplacer(" ", "", "\t", "", "\n", "", "\r", "", "\v", "", "\f", "").Replace(input)
+
+	if input == "" {
+		return "", fmt.Errorf("error in operands: %w", errorEmptyInput)
+	}
+
+	values := strings.Split(input, "+")
+
+	if len(values) == 1 {
+		lastIndex := strings.LastIndex(input, "-")
+
+		switch {
+		case lastIndex <= 0:
+			return "", fmt.Errorf("error in operands: %w", errorNotTwoOperands)
+		case len(input)-1 == lastIndex:
+			return "", fmt.Errorf("error in operands: %w", errorNotTwoOperands)
+		default:
+			return calculate(input[:lastIndex], input[lastIndex:])
+		}
+	}
+
+	if len(values) != 2 {
+		return "", fmt.Errorf("error in operands: %w", errorNotTwoOperands)
+	}
+
+	return calculate(values[0], values[1])
+}
+
+func calculate(value1, value2 string) (string, error) {
+	number1, err1 := strconv.Atoi(value1)
+	if err1 != nil {
+		return "", fmt.Errorf("error in calculation: %w", err1)
+	}
+
+	number2, err2 := strconv.Atoi(value2)
+	if err2 != nil {
+		return "", fmt.Errorf("error in calculation: %w", err2)
+	}
+
+	return strconv.Itoa(number1 + number2), nil
 }
